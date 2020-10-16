21166a75e02de146fdc9ae8d0a2df56b8abad531
unknown <tapia.villasana.diego@gmail.comgit config --global user.name Tapia>
2020-10-15 23:49:05 -0500

    Hola Mundo

---
+++ Entrada Datos/EntradaDatos.java
@@ -0,0 +1,10 @@
+class  EntradaDatos{
+    /*Este programa nos va a servir para poder meter datos 
+    */
+    public static void main(String[] args ){
+        String nombre;
+        System.out.println("Dime tu nombre nwn/ ");
+        nombre=System.console().readLine();
+        System.out.println("Bienvenido: "+nombre+"a programacion");
+    }
+}
---
+++ Hola Mundo/HolaMundo.java
@@ -0,0 +1,12 @@
+class HolaMundo{
+    
+
+    public static void main(String[] args){
+        
+        String nombre = "Cosita kawaii";
+        
+        System.out.println("Por fis di Hola mundo");
+        System.out.println(nombre);
+    }
+
+}
